39ac683ff814f86a4a9600b072db06d2e8801cb9
Prashan Prajapati <officialprashan@gmail.com>
2022-07-21 14:45:47 +0545

    timer not in control :P

--- script.js
+++ script.js
@@ -1,17 +1,138 @@
 "use strict";
 
-console.log(`I am console.`);
+console.log("sync");
 
-const tabItem = document.querySelector(".tab-item");
-const startButton = document.querySelector(".start");
+//1. Select all the tabs from tab list
+const PomodoroTab = document.getElementById("pomodoro-tab");
+const ShortBreakTab = document.getElementById("short-break-tab");
+const LongBreakTab = document.getElementById("long-break-tab");
 
-startButton.addEventListener("click", function () {
-  startButton.classList.add("active");
-  startButton.textContent = "Go On";
-  startButton.style.color = "black";
+const PomodoroContent = document.getElementById("pomodoro-content");
+const ShortBreakContent = document.getElementById("shortbreak-content");
+const LongBreakContent = document.getElementById("longbreak-content");
+
+const pomoTimer = document.getElementById("pomoTimer");
+const shortTimer = document.getElementById("shortTimer");
+const longTimer = document.getElementById("longTimer");
+
+const pStartTimer = document.getElementById("pomoStartTimer");
+const sStartTimer = document.getElementById("shortStartTimer");
+const lStartTimer = document.getElementById("longStartTimer");
+
+const Pause = document.getElementById("Pause");
+
+//Text from js
+pStartTimer.textContent = "START";
+sStartTimer.textContent = "START";
+lStartTimer.textContent = "START";
+
+//Add click event listener on each tab
+PomodoroTab.addEventListener("click", function () {
+  console.log("Pomodoro");
+
+  //TAB SELECT
+  PomodoroTab.classList.add("active");
+  ShortBreakTab.classList.remove("active");
+  LongBreakTab.classList.remove("active");
+
+  //TAB CONTENT
+  PomodoroContent.classList.add("active");
+  ShortBreakContent.classList.remove("active");
+  LongBreakContent.classList.remove("active");
 });
 
-tabItem.addEventListener("click", function () {
-  tabItem.classList.add("active");
-  console.log("tab-item");
+ShortBreakTab.addEventListener("click", () => {
+  console.log("short break");
+
+  //TAB SELECT
+  ShortBreakTab.classList.add("active");
+  PomodoroTab.classList.remove("active");
+  LongBreakTab.classList.remove("active");
+
+  //TAB CONTENT
+  ShortBreakContent.classList.add("active");
+  PomodoroContent.classList.remove("active");
+  LongBreakContent.classList.remove("active");
+});
+
+LongBreakTab.addEventListener("click", () => {
+  console.log("long break");
+
+  //TAB SELECT
+  LongBreakTab.classList.add("active");
+  ShortBreakTab.classList.remove("active");
+  PomodoroTab.classList.remove("active");
+
+  //TAB CONTENT
+  LongBreakContent.classList.add("active");
+  ShortBreakContent.classList.remove("active");
+  PomodoroContent.classList.remove("active");
+});
+
+//Timer function
+const timer = (seconds, contentDisplay, timerControl) => {
+  const now = Date.now();
+  const then = now + seconds * 1000;
+
+  let timeInterval = setInterval(() => {
+    if (timerControl.textContent === "START") {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      displayTimer(secondsLeft, contentDisplay);
+      console.log(secondsLeft);
+
+      //timer end condition
+      if (secondsLeft < 1) {
+        clearInterval(timeInterval);
+      }
+    }
+  }, 1000);
+  timerControl.addEventListener("click", function () {
+    if (timerControl.textContent === "START") {
+      timerControl.textContent = "STOP";
+      console.log("STOP EVENT");
+    }
+    if (timerControl.textContent === "STOP") {
+      timerControl.addEventListener("click", function () {
+        timerControl.textContent = "START";
+        console.log("START EVENT");
+      });
+    }
+  });
+};
+
+function displayTimer(seconds, element) {
+  const minute = Math.floor(seconds / 60);
+  const second = seconds % 60;
+
+  console.log({ minute, second });
+  element.innerText = minute + " : " + second;
+}
+
+//Add button event listener
+pStartTimer.addEventListener("click", function () {
+  let PomodoroTotalTime = 1500;
+  timer(PomodoroTotalTime, pomoTimer, pStartTimer);
+});
+
+sStartTimer.addEventListener("click", function () {
+  let shortTotalTime = 300;
+  if (sStartTimer.textContent === "START") {
+    sStartTimer.textContent = "STOP";
+    timer(shortTotalTime, shortTimer);
+  } else if (sStartTimer.textContent === "STOP") {
+    sStartTimer.textContent = "START";
+    clearInterval(timer.timeInterval);
+    console.log("stop");
+  }
+});
+lStartTimer.addEventListener("click", function () {
+  let LongTotalTime = 600;
+  if (lStartTimer.textContent === "START") {
+    lStartTimer.textContent = "STOP";
+    timer(LongTotalTime, longTimer);
+  } else if (lStartTimer.textContent === "STOP") {
+    lStartTimer.textContent = "START";
+    clearInterval(timer.timeInterval);
+    console.log("stop");
+  }
 });
